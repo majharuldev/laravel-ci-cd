bcd2463d085921ee93bb54c412a0bb54acc06056
majharul <majharul.paperles@gmail.com>
2026-08-15 16:15:06 +0600

    Test live CI/CD

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cicd-test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'CI/CD is working',
+    ]);
+});
